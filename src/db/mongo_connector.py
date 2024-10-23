7cdac9587cde66d7fff12d0f3cd37c41597ba46f
Muhammad <zohaibkhattak6@gmail.com>
2024-10-24 01:13:25 +0500

    some-product-changes-added

--- src/db/mongo_connector.py
+++ src/db/mongo_connector.py
@@ -1,30 +1,33 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-# MongoDB connection URI for Atlas
-uri = "mongodb+srv://zohaibkhattak6:Qk3ewCUfFumPMHnG@cluster0.tmfxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+URI = "mongodb+srv://zohaibkhattak6:Qk3ewCUfFumPMHnG@cluster0.tmfxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME='product_scraper'
+COLLECTION_NAME='products'
 
 def get_db_connection():
     try:
-        client = MongoClient(uri, server_api=ServerApi('1'))
-        db = client['product_scraper']
+        client = MongoClient(URI, server_api=ServerApi('1'))
+        db = client[DB_NAME]
         return db
     except Exception as e:
         print(f"Error connecting to database: {e}")
         return None
 
 def store_product(product_data):
-    # Connect to the database using the get_db_connection function
     db = get_db_connection()
     
-    # Check if the connection was successful
     if db is not None:
-        # Insert product data into a collection
-        collection = db['products']
-        try:
-            result = collection.insert_one(product_data)
-            print(f"Product inserted with ID: {result.inserted_id}")
-        except Exception as e:
-            print(f"Error inserting product data: {e}")
+        existing_product = db[COLLECTION_NAME].find_one({"productId": product_data['productId']})
+        
+        if existing_product:
+            print("Product already exists in the database.")
+        else:
+            collection = db[COLLECTION_NAME]
+            try:
+                result = collection.insert_one(product_data)
+                print(f"Product inserted with ID: {result.inserted_id}")
+            except Exception as e:
+                print(f"Error inserting product data: {e}")
     else:
         print("Failed to connect to the database.")
--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -8,40 +8,43 @@ def scrape_product_data(product_url):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         
+        print({soup})
+        
         # Find the script tag that contains the product data
         script_tag = soup.find('script', text=re.compile('magentoStorefrontEvents.context.setProduct'))
-        
-        print({script_tag})
-        
+                
         if script_tag:
-            # Extract the script content
             script_content = script_tag.string
             
-            # Extract product data using regex
             product_data = {}
-            
-            # Extract product name
             name_match = re.search(r'"name":"(.*?)"', script_content)
             if name_match:
                 product_data['name'] = name_match.group(1)
             
-            # Extract product ID
             id_match = re.search(r'"productId":(\d+)', script_content)
             if id_match:
                 product_data['productId'] = id_match.group(1)
             
-            # Extract SKU
             sku_match = re.search(r'"sku":"(.*?)"', script_content)
             if sku_match:
                 product_data['sku'] = sku_match.group(1)
             
-            # Extract canonical URL
             url_match = re.search(r'"canonicalUrl":"(.*?)"', script_content)
             if url_match:
-                # Canonical URL contains escaped slashes, so we need to unescape them
                 product_data['canonicalUrl'] = url_match.group(1).replace('\\/', '/')
             
-            # If all data was found, return it as a single object
+            categories_match = re.search(r'"categories":\[(.*?)\]', script_content)
+            if categories_match:
+                categories = re.findall(r'"(.*?)"', categories_match.group(1))
+                product_data['categories'] = categories
+            
+            pricing_match = re.search(r'"pricing":(.*?)}', script_content)
+            if pricing_match:
+                product_data['pricing'] = pricing_match.group(1)
+            
+            image_url_match = re.search(r'"mainImageUrl":"(.*?)"', script_content)
+            if image_url_match:
+                product_data['mainImageUrl'] = image_url_match.group(1).replace('\\/', '/')
             if product_data:
                 print(f"Scraped product data: {product_data}")
                 return product_data
